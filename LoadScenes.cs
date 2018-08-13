bf31edd3a257f544fb0a7595cb688cc9df9501fd
Praven P Jose <pravenpjose@gmail.com>
2018-08-13 09:34:10 +0530

    Add files via upload

---
+++ LoadScenes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour {
+    public float loadProgress;
+    public string sceneName;
+    public bool load;
+    AsyncOperation async;
+
+    void Start ()
+    {
+        StartCoroutine(LoadNewScene());
+	}
+	
+	
+	void Update ()
+    {
+        async.allowSceneActivation = load;
+    }
+
+
+    IEnumerator LoadNewScene()
+    {
+        
+        // Set priority low and load asynchronously to help performance while loading
+        Application.backgroundLoadingPriority = ThreadPriority.Low;
+        async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+        // Wait until Unity has finished loading the scene.
+        // Wth allowSceneActivation = false Unity won't fully load the scene and will
+        // and async.progress will only go as far as 90%
+        while (!async.isDone)
+        {
+             loadProgress = async.progress;
+
+            if (loadProgress >= 0.9f)
+            {
+                // Almost done.
+                break;
+            }
+
+            yield return null;
+        }
+        // Finish loading the scene
+        yield return null;
+        yield return async;
+        Debug.Log("Loading complete");
+    }
+}
---
+++ WaveCreater.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class WaveCreater : MonoBehaviour
+{
+    public enum SpawnState
+    {
+        SPAWNING,
+        WAITING,
+        COUNTING,
+        OVER
+    }
+     SpawnState spawnState = SpawnState.COUNTING;
+
+    [System.Serializable]
+	public class Wave
+    {
+        public string name;
+        public GameObject[] enemy;
+        public int count;
+        public float rate;
+    }
+    public Wave[] waves;
+    public float timeBetweenWaves=5f;
+    public Transform[] spwanPoints;
+    public UnityEvent allWaveOver,oneWaveCompleted;
+
+    int nextWave = 0;
+    float searchEnemy=1f;
+    float waveCountDown;
+
+    void Start ()
+    {
+        waveCountDown = timeBetweenWaves;
+    }
+	
+
+	void Update ()
+    {
+        switch(spawnState)
+        {
+            case SpawnState.OVER:
+                {
+                    break;
+                }
+            case SpawnState.WAITING:
+                {
+                    if (!EnemyIsAlive())
+                    {
+                        WaveCompleted();
+                    }
+                    break;
+                }
+            default:
+                {
+                    if (waveCountDown <= 0)
+                    {
+                        if (spawnState != SpawnState.SPAWNING)
+                        {
+                            StartCoroutine(SpwanWave(waves[nextWave]));
+                            
+                        }
+                    }
+                    else
+                    {
+                        waveCountDown -= Time.deltaTime;
+                    }
+                    break;
+                }
+        }
+
+	}
+
+    IEnumerator SpwanWave(Wave _wave)
+    {
+        print("Spawning Wave"+_wave.name);
+        spawnState = SpawnState.SPAWNING;
+        for(int i=0;i<_wave.count;i++)
+        {
+            Transform enemy = _wave.enemy[Random.Range(0, _wave.enemy.Length)].transform;
+            SpawnEnemy(enemy);
+            yield return new WaitForSeconds(_wave.rate);
+        }
+        spawnState = SpawnState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Transform tempPos = spwanPoints[Random.Range(0, spwanPoints.Length)];
+        Instantiate(_enemy, tempPos.position, Quaternion.identity);
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchEnemy -= Time.deltaTime;
+       
+        if (searchEnemy <= 0)
+        {
+            searchEnemy = 1f;
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    void WaveCompleted()
+    {
+        print("Wave Completed");
+        spawnState = SpawnState.COUNTING;
+        waveCountDown = timeBetweenWaves;
+        if((nextWave+1)>=waves.Length)
+        {
+            print("All Waves Completed");
+            allWaveOver.Invoke();
+            spawnState = SpawnState.OVER;
+            StopAllCoroutines();
+        }
+        else
+        {
+            oneWaveCompleted.Invoke();
+            nextWave++;
+        }
+       
+    }
+}
